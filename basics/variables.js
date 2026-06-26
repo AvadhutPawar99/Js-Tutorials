336ebea18a9f3cbbae61397d75f0d7041b18677e
Avadhut Pawar <pawar.avadhut221999@gmail.com>
2026-06-26 13:14:14 +0000

    Container config

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 225533
+let accountEmail ="AvadhutPawar25@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 1 // Not Allowed
+
+accountEmail = 'A@gmail.com'
+accountPassword = "123456788"
+accountCity = 'Nashik'
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
