4c722195a72fcc5b0f973efe8f4a5b5dff11af9c
Avadhut Pawar <pawar.avadhut221999@gmail.com>
2026-06-29 13:40:10 +0000

    ConversionOperation

---
+++ basics/Conversion_Operation.js
@@ -0,0 +1,24 @@
+// let score =  false
+
+// // const {score} = req.body
+
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+// let isLoggedIn = "Avi"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
