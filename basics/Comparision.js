f5ea3292788e8d2e09b0887a062f3aab5b43e54d
Avadhut Pawar <pawar.avadhut221999@gmail.com>
2026-06-30 09:39:51 +0000

    Comparision

---
+++ basics/Comparision.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//---- Different Data Type -----//
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// ---- Null Value ----------//
+
+// console.log(null > 0);
+// console.log(null <= 0);
+// console.log(null == 0);
+// console.log(null != 0);
+
+//---- Undefined Value ---------//
+
+// console.log(undefined > 0);
+// console.log(undefined <= 0);
+// console.log(undefined == 0);
+// console.log(undefined != 0);
+
+// === --> Strict Check <-- ===\\
+console.log("2" === 2);
