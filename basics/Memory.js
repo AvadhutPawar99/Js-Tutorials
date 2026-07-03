6df06cd50ec25b70c7425a0234ee452f35c92168
Avadhut Pawar <pawar.avadhut221999@gmail.com>
2026-07-03 11:22:45 +0000

    Memory

--- basics/Memory.js
+++ basics/Memory.js
@@ -1,1 +1,24 @@
 // Stack , Heap
+
+let myYoutubeName = "CoopYT20"
+
+let anotherName = myYoutubeName
+
+anotherName = 'Coop'
+
+console.log(anotherName);
+console.log(myYoutubeName);
+
+
+let User1 = {
+    email : "Aviraj@email.co.in",
+    upi : "abi@axl"
+}
+
+let User2 = User1
+
+User2.email = 'Avinav@gamil.co.in'
+
+console.log(User2);
+console.log(User1);
+
