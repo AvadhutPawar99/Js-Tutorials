ac36bf979705e3f72d4dd0932befaa8f3c8224b7
Avadhut Pawar <pawar.avadhut221999@gmail.com>
2026-07-07 12:18:24 +0000

    Number&Math

---
+++ basics/Numbers_&_Maths.js
@@ -0,0 +1,42 @@
+// const score = 40000
+// console.log(score);
+
+
+// const balance = new Number(30000)
+
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(4));
+
+// const otherNumber = 3.145645
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hunders = 10000000000
+
+// console.log(hunders.toLocaleString('en-IN'));
+
+                                            //        --------- Maths ----------       //
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.66));
+// console.log(Math.ceil(42.56));
+// console.log(Math.floor(42.56));
+
+// console.log(Math.min(4,3,2,0,6));
+// console.log(Math.max(4,3,2,0,6));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10 +1));
+
+
+const min = 10
+const max = 25
+
+console.log((Math.floor(Math.random() * (max-min + 1))+ min))
